07b9dcd3f4b2b392626259e103fdd1002985a2f4
Rohit Joshi <rohit14joshi1993@gmail.com>
2023-09-06 20:01:21 +0100

    updated componentDidUpdate in src/Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -66,9 +66,9 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
