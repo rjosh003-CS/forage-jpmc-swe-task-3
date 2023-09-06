4192a3dcdf7f6aa3a6895d7fbc6507509237d354
Rohit Joshi <rohit14joshi1993@gmail.com>
2023-09-06 19:57:27 +0100

    updated attributes in src/Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -48,10 +48,18 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+        // stock: 'distinctcount',
+        // top_ask_price: 'avg',
+        // top_bid_price: 'avg',
+        // timestamp: 'distinct count',
+
+        price_abc : 'avg',
+        price_def : 'avg',
+        ratio: 'avg',
+        timestamp : 'distinct count',
+        upper_bound : 'avg',
+        lower_bound : 'avg',
+        trigger_alert : 'avg',
       }));
     }
   }
